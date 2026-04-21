22f1bd21477d972259de40190f8efa816fca7325
Sturdy7435 <172964918+Sturdy7435@users.noreply.github.com>
2026-04-21 21:24:28 +0200

    feat: implement track feed

---
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/dto/endpoints/GetSimilarSongsDto.kt
@@ -0,0 +1,25 @@
+package dev.brahmkshatriya.echo.extension.dto.endpoints
+
+import dev.brahmkshatriya.echo.extension.dto.types.ErrorDto
+import dev.brahmkshatriya.echo.extension.dto.types.SongDto
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetSimilarSongsDto(
+    @SerialName("subsonic-response")
+    val subsonicResponse: SubsonicResponseDto
+) {
+    @Serializable
+    data class SubsonicResponseDto(
+        val status: String,
+        val error: ErrorDto? = null,
+
+        val similarSongs2: SimilarSongsDto? = null,
+    ) {
+        @Serializable
+        data class SimilarSongsDto(
+            val song: List<SongDto>,
+        )
+    }
+}
