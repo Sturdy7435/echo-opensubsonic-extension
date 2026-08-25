5f7b5262270b310359371c53b0204bdc6f66ce11
Sturdy7435 <172964918+Sturdy7435@users.noreply.github.com>
2026-08-25 11:37:29 +0200

    feat: add a genres tab to the search feed

--- ext/src/main/java/dev/brahmkshatriya/echo/extension/clients/searchfeed/SearchFeedClientImpl.kt
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/clients/searchfeed/SearchFeedClientImpl.kt
@@ -6,6 +6,8 @@ import dev.brahmkshatriya.echo.common.models.Feed
 import dev.brahmkshatriya.echo.common.models.Feed.Companion.toFeedData
 import dev.brahmkshatriya.echo.common.models.Shelf
 import dev.brahmkshatriya.echo.common.models.Tab
+import dev.brahmkshatriya.echo.extension.service.genre.GenreService.createGenreFeed
+import dev.brahmkshatriya.echo.extension.service.genre.GenreService.getGenres
 import dev.brahmkshatriya.echo.extension.service.search.SearchService
 import dev.brahmkshatriya.echo.extension.service.search.SearchService.search
 
@@ -17,13 +19,38 @@ class SearchFeedClientImpl : SearchFeedClient {
             search(query, 20, 20, 20)
         }
 
+        val genresData = if (query.isBlank()) {
+            getGenres()
+        } else {
+            getGenres().filter { it.contains(query.trim(), ignoreCase = true) }
+        }
+
         return Feed(
-            listOf("Tracks", "Albums", "Artists").map { Tab(it, it) },
+            listOf("Tracks", "Albums", "Artists", "Genres").map { Tab(it, it) },
         ) { tab ->
             val pagedData: PagedData.Single<Shelf> = when (tab?.id) {
-                "Tracks" -> PagedData.Single { data.tracks?.map { it.toShelf() as Shelf } ?: listOf() }
-                "Albums" -> PagedData.Single { data.albums?.map { it.toShelf() as Shelf } ?: listOf() }
-                "Artists" -> PagedData.Single { data.artists?.map { it.toShelf() as Shelf } ?: listOf() }
+                "Tracks" -> PagedData.Single {
+                    data.tracks?.map { it.toShelf() as Shelf } ?: listOf()
+                }
+
+                "Albums" -> PagedData.Single {
+                    data.albums?.map { it.toShelf() as Shelf } ?: listOf()
+                }
+
+                "Artists" -> PagedData.Single {
+                    data.artists?.map { it.toShelf() as Shelf } ?: listOf()
+                }
+
+                "Genres" -> PagedData.Single {
+                    genresData.map {
+                        Shelf.Category(
+                            id = it.lowercase().replace(" ", ""),
+                            title = it,
+                            feed = createGenreFeed(it),
+                        )
+                    }
+                }
+
                 else -> throw IllegalArgumentException("Unknown tab")
             }
             pagedData.toFeedData()
